69dca5e016eb02fdff6ce76e3aa069217cf2c673
Noah Parsons <nomapa223@gmail.com>
2025-11-12 08:26:50 -0700

    Update streamlit_quantum_photosynthesis.py

--- streamlit_quantum_photosynthesis.py
+++ streamlit_quantum_photosynthesis.py
@@ -9,7 +9,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from quantum_photosynthesis import *
+from quantum_photosynthesis.py import *
 
 # Configure Streamlit page
 st.set_page_config(
